243623468b2516f3c2785bbfbd7e3bc2cb5fdbb8
sundara.v.rajendran <clr.sundar@gmail.com>
2025-07-02 12:22:30 +0530

    initial serverless commit

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,16 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import serverless from 'serverless-http';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
 dotenv.config();
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/hello', (req, res) => {
-        res.json({ message: 'Hello from Lambda Express!' });
-    }
-);
+app.get('/api/hello', (req: Request, res: Response) => {
+  res.json({ message: 'Hello from Lambda Express with ESM!' });
+});
 
 export const handler = serverless(app);
