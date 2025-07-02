d70dd851415cf4a64fa46c011c430e2cbe60e901
sundara.v.rajendran <clr.sundar@gmail.com>
2025-07-02 13:03:11 +0530

    modified CI/CD

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,13 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import serverless from 'serverless-http';
 import cors from 'cors';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/hello', (req: Request, res: Response) => {
-  res.json({ message: 'Hello from Lambda Express with ESM!' });
+app.get('/api/hello', (_req, res) => {
+  res.json({ message: 'Hello from Lambda Express!' });
 });
 
 export const handler = serverless(app);
